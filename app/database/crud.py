364955ea098ba8b4ef72839887ad98b3b8f15fef
kosorz <kosorz.artur@gmail.com>
2020-10-02 08:28:06 +0200

    Make crud actions more DRY

--- app/database/crud.py
+++ app/database/crud.py
@@ -2,10 +2,27 @@ from sqlalchemy.orm import Session
 from sqlalchemy import update
 
 from . import models
-from ..routers.util.schemas import UserCreate, Resume, ResumeCreate, Info, SkillsGroup, Experience, ExperienceUnit
+from ..routers.util.schemas import UserCreate, Resume, ResumeCreate, Info, SkillsGroup, Experience, ExperienceUnit, Skills
 from ..routers.auth import get_password_hash
 
 
+def finalize_create(db: Session, resource):
+    db.add(resource)
+    db.commit()
+    db.refresh(resource)
+    return resource
+
+
+def finalize_update(db: Session, old_resource, updated_resource):
+    for key, value in updated_resource:
+        setattr(old_resource, key, value)
+
+    db.commit()
+    db.refresh(old_resource)
+    return old_resource
+
+
+# Users
 def get_user_by_username(db: Session, username: str):
     return db.query(
         models.User).filter(models.User.username == username).first()
@@ -17,59 +34,29 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 def create_user(db: Session, user: UserCreate):
     hashed_password = get_password_hash(user.password)
-    db_user = models.User(username=user.username,
-                          hashed_password=hashed_password)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+    return finalize_create(
+        db, models.User(username=user.username,
+                        hashed_password=hashed_password))
 
 
+# Resumes
 def get_resume(db: Session, resume_id: int):
     return db.query(
         models.Resume).filter(models.Resume.id == resume_id).first()
 
 
 def update_resume(db: Session, resume: Resume):
-    old_resume = db.query(
-        models.Resume).filter(models.Resume.id == resume.id).first()
-
-    for key, value in resume:
-        setattr(old_resume, key, value)
-
-    db.commit()
-    db.refresh(old_resume)
-    return old_resume
+    return finalize_update(db, get_resume(db, resume.id), resume)
 
 
 def create_user_resume(db: Session, resume: ResumeCreate, user_id: int):
-    db_resume = models.Resume(**resume.dict(), owner_id=user_id)
-    db.add(db_resume)
-    db.commit()
-    db.refresh(db_resume)
-    return db_resume
+    return finalize_create(db, models.Resume(**resume.dict(),
+                                             owner_id=user_id))
 
 
+# Infos
 def create_resume_info(db: Session, resume_id: int, name: str):
-    db_info = models.Info(resume_id=resume_id, name=name)
-    db.add(db_info)
-    db.commit()
-    db.refresh(db_info)
-    return db_info
-
-
-def update_resume_info(db: Session, info: Info):
-    old_info = db.query(
-        models.Info).filter(models.Info.resume_id == info.resume_id).first()
-
-    print(old_info)
-
-    for key, value in info:
-        setattr(old_info, key, value)
-
-    db.commit()
-    db.refresh(old_info)
-    return old_info
+    return finalize_create(db, models.Info(resume_id=resume_id, name=name))
 
 
 def get_resume_info(db: Session, resume_id: int):
@@ -77,37 +64,26 @@ def get_resume_info(db: Session, resume_id: int):
         models.Info).filter(models.Info.resume_id == resume_id).first()
 
 
+def update_resume_info(db: Session, info: Info):
+    return finalize_update(db, get_resume_info(db, info.resume_id), info)
+
+
+# Skills
 def get_resume_skills(db: Session, resume_id: int):
     return db.query(
         models.Skills).filter(models.Skills.resume_id == resume_id).first()
 
 
 def create_resume_skills(db: Session, resume_id: int):
-    db_skills = models.Skills(resume_id=resume_id)
-    db.add(db_skills)
-    db.commit()
-    db.refresh(db_skills)
-    return db_skills
+    return finalize_create(db, models.Skills(resume_id=resume_id))
 
 
-def update_resume_skills(db: Session, skills: Info):
-    old_skills = db.query(models.Skills).filter(
-        models.Skills.resume_id == skills.resume_id).first()
-
-    for key, value in skills:
-        setattr(old_skills, key, value)
-
-    db.commit()
-    db.refresh(old_skills)
-    return old_skills
+def update_resume_skills(db: Session, skills: Skills):
+    return finalize_update(db, get_resume_skills(db, skills.resume_id), skills)
 
 
 def create_skills_group(db: Session, skills_id: int):
-    db_skills_group = models.SkillsGroup(skills_id=skills_id)
-    db.add(db_skills_group)
-    db.commit()
-    db.refresh(db_skills_group)
-    return db_skills_group
+    return finalize_create(db, models.SkillsGroup(skills_id=skills_id))
 
 
 def get_skills_group(db: Session, group_id: int):
@@ -116,15 +92,13 @@ def get_skills_group(db: Session, group_id: int):
 
 
 def update_skills_group(db: Session, skills_group: SkillsGroup):
-    old_skills_group = db.query(models.SkillsGroup).filter(
-        models.SkillsGroup.id == skills_group.id).first()
+    return finalize_update(db, get_skills_group(db, skills_group.id),
+                           skills_group)
 
-    for key, value in skills_group:
-        setattr(old_skills_group, key, value)
 
-    db.commit()
-    db.refresh(old_skills_group)
-    return old_skills_group
+# Experiences
+def create_resume_experience(db: Session, resume_id: int):
+    return finalize_create(db, models.Experience(resume_id=resume_id))
 
 
 def get_resume_experience(db: Session, resume_id: int):
@@ -132,46 +106,21 @@ def get_resume_experience(db: Session, resume_id: int):
         models.Experience.resume_id == resume_id).first()
 
 
-def create_resume_experience(db: Session, resume_id: int):
-    db_experience = models.Experience(resume_id=resume_id)
-    db.add(db_experience)
-    db.commit()
-    db.refresh(db_experience)
-    return db_experience
-
-
 def update_resume_experience(db: Session, skills: Info):
-    old_experience = db.query(models.Experience).filter(
-        models.Experience.resume_id == skills.resume_id).first()
-
-    for key, value in skills:
-        setattr(old_experience, key, value)
-
-    db.commit()
-    db.refresh(old_experience)
-    return old_experience
+    return finalize_update(db, get_resume_experience(db, skills.resume_id),
+                           skills)
 
 
 def create_experience_unit(db: Session, experience_id: int):
-    db_experience_unit = models.ExperienceUnit(experience_id=experience_id)
-    db.add(db_experience_unit)
-    db.commit()
-    db.refresh(db_experience_unit)
-    return db_experience_unit
+    return finalize_create(db,
+                           models.ExperienceUnit(experience_id=experience_id))
 
 
-def get_experience_unit(db: Session, group_id: int):
+def get_experience_unit(db: Session, unit_id: int):
     return db.query(models.ExperienceUnit).filter(
-        models.ExperienceUnit.id == group_id).first()
+        models.ExperienceUnit.id == unit_id).first()
 
 
-def update_experience_unit(db: Session, skills_group: ExperienceUnit):
-    old_experience_unit = db.query(models.ExperienceUnit).filter(
-        models.ExperienceUnit.id == skills_group.id).first()
-
-    for key, value in skills_group:
-        setattr(old_experience_unit, key, value)
-
-    db.commit()
-    db.refresh(old_experience_unit)
-    return old_experience_unit
+def update_experience_unit(db: Session, experience_unit: ExperienceUnit):
+    return finalize_update(db, get_experience_unit(db, experience_unit.id),
+                           experience_unit)
